8fff5acbdba8bad86baa2be8097e6351c2a6290f
David Liendo <D.A.L.F20@hotmail.com>
2023-10-26 12:09:02 -0400

    added box

--- PicturePickerApp/Dashboard.Designer.cs
+++ PicturePickerApp/Dashboard.Designer.cs
@@ -44,26 +44,27 @@ namespace PicturePickerApp
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(377, 32);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(251, 21);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(912, 636);
+            this.pictureBox1.Size = new System.Drawing.Size(608, 413);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // uploadButton1
             // 
-            this.uploadButton1.Location = new System.Drawing.Point(963, 671);
-            this.uploadButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.uploadButton1.Location = new System.Drawing.Point(642, 436);
+            this.uploadButton1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.uploadButton1.Name = "uploadButton1";
-            this.uploadButton1.Size = new System.Drawing.Size(108, 34);
+            this.uploadButton1.Size = new System.Drawing.Size(72, 22);
             this.uploadButton1.TabIndex = 1;
             this.uploadButton1.Text = "Upload";
             this.uploadButton1.UseVisualStyleBackColor = true;
@@ -71,28 +72,28 @@ namespace PicturePickerApp
             // 
             // uploadText
             // 
-            this.uploadText.Location = new System.Drawing.Point(510, 671);
-            this.uploadText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.uploadText.Location = new System.Drawing.Point(340, 436);
+            this.uploadText.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.uploadText.Name = "uploadText";
-            this.uploadText.Size = new System.Drawing.Size(446, 26);
+            this.uploadText.Size = new System.Drawing.Size(299, 20);
             this.uploadText.TabIndex = 2;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(377, 675);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveButton.Location = new System.Drawing.Point(251, 439);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(84, 29);
+            this.saveButton.Size = new System.Drawing.Size(56, 19);
             this.saveButton.TabIndex = 3;
             this.saveButton.Text = "Save";
             this.saveButton.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(32, 32);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Location = new System.Drawing.Point(21, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 26);
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
             this.textBox1.TabIndex = 4;
             // 
             // errorProvider1
@@ -102,23 +103,33 @@ namespace PicturePickerApp
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(106, 86);
+            this.label1.Location = new System.Drawing.Point(71, 56);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(885, 20);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(207, 414);
+            this.textBox2.TabIndex = 6;
             // 
             // Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1656, 724);
+            this.ClientSize = new System.Drawing.Size(1114, 471);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.uploadText);
             this.Controls.Add(this.uploadButton1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Dashboard";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -137,5 +148,6 @@ namespace PicturePickerApp
         private TextBox textBox1;
         private ErrorProvider errorProvider1;
         private Label label1;
+        private TextBox textBox2;
     }
 }
